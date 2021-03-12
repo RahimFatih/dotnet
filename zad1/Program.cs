af6c57ca28f149ed647f0239d01c8a2ea634742c
korewatabit <smoltabit@gmail.com>
2021-03-12 17:02:26 +0100

    Szkielet

--- zad1/Program.cs
+++ zad1/Program.cs
@@ -1,11 +1,59 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace dotnet
 {
-    class Program
+
+
+    public class plecak
+    {
+        List<przedmiot> przedmioty = new List<przedmiot>();
+        private int wagaMaksymalna;
+        
+        public plecak(int waga)
+        {
+            wagaMaksymalna=waga;
+        }
+        public void dodajPrzedmiot(przedmiot nowy)
+        {
+            przedmioty.Add(nowy);
+        }
+        public void pokazPrzedmioty()
+        {
+            Console.WriteLine(przedmioty[0].wagaPrzedmiotu);
+        }
+    }
+    
+    //Klasa Przedmiot TODO
+    //•Konstruktor w postaci xD= new przedmiot(54)
+    public class przedmiot
+    {   
+        public int wagaPrzedmiotu;
+        private int wartoscPrzedmiotu;
+        private bool stanPrzedmiotu;
+        private float stosunekWartoscWaga;
+    
+
+        
+        public przedmiot(int waga, int wartosc)
+        {
+            wagaPrzedmiotu = waga;
+            wartoscPrzedmiotu = wartosc;
+            stosunekWartoscWaga=wartosc/waga;
+        }
+        
+        public bool ustawStan(bool stan)
+        {
+            stanPrzedmiotu = stan;
+            return stan;
+        }
+    }
+        class Program
     {
         static void Main(string[] args)
         {
+            plecak mojPlecak = new plecak(54);
+            mojPlecak.dodajPrzedmiot(new przedmiot(10,5));
+            mojPlecak.pokazPrzedmioty();
             Console.WriteLine("Test!");
         }
     }
