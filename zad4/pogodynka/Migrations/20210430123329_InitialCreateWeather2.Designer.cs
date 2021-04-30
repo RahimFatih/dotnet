75995a908eb547232bd629c466e0a1bde497a4b3
RahimFatih <65657905+RahimFatih@users.noreply.github.com>
2021-04-30 16:29:59 +0200

    t

--- zad4/pogodynka/Migrations/20210430123329_InitialCreateWeather2.Designer.cs
+++ zad4/pogodynka/Migrations/20210430123329_InitialCreateWeather2.Designer.cs
@@ -2,14 +2,16 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace pogodynka.Migrations
 {
     [DbContext(typeof(RazorPagesGameContext))]
-    partial class RazorPagesGameContextModelSnapshot : ModelSnapshot
+    [Migration("20210430123329_InitialCreateWeather2")]
+    partial class InitialCreateWeather2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ zad4/pogodynka/Migrations/20210430123329_InitialCreateWeather2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pogodynka.Migrations
+{
+    public partial class InitialCreateWeather2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Weather",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    weatherDescription = table.Column<string>(type: "TEXT", nullable: true),
+                    mainWeatherTemp = table.Column<float>(type: "REAL", nullable: false),
+                    visibility = table.Column<float>(type: "REAL", nullable: false),
+                    dt = table.Column<int>(type: "INTEGER", nullable: false),
+                    timezone = table.Column<int>(type: "INTEGER", nullable: false),
+                    name = table.Column<string>(type: "TEXT", nullable: true),
+                    cod = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Weather", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Weather");
+        }
+    }
+}
